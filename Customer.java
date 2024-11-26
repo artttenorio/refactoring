3c57c75fcc59687c33b090107588b9b8a9767c30
Arthur Tenorio <arthurtenorio0101@gmail.com>
2024-11-26 15:04:43 -0300

    Commit 4

--- Customer.java
+++ Customer.java
@@ -1,47 +1,50 @@
 import java.util.Vector;
 
-public class Customer {
-    private String name;
-    private Vector<Rental> rentals = new Vector<>();
 
-    public Customer(String name) {
-        this.name = name;
-    }
-
-    public void addRental(Rental rental) {
-        rentals.add(rental);
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String statement() {
-        double totalAmount = 0;
-        int frequentRenterPoints = 0;
-        StringBuilder result = new StringBuilder("Rental Record for " + getName() + "\n");
-
-        for (Rental rental : rentals) {
-            double thisAmount = amountFor(rental);
-
-            // Adiciona pontos de aluguel frequente
-            frequentRenterPoints++;
-            if (rental.getMovie().getPriceCode() == Movie.NEW_RELEASE && rental.getDaysRented() > 1) {
-                frequentRenterPoints++;
-            }
-
-            // Adiciona detalhes do aluguel ao relatório
-            result.append(String.format("\t%s\t%.2f\n", rental.getMovie().getTitle(), thisAmount));
-            totalAmount += thisAmount;
-        }
-
-        // Adiciona os totais ao final do relatório
-        result.append(String.format("Amount owed is %.2f\n", totalAmount));
-        result.append(String.format("You earned %d frequent renter points", frequentRenterPoints));
-
-        return result.toString();
-    }
-
+    public class Customer {
+      private String name;
+      private Vector<Rental> rentals = new Vector<>();
+  
+      public Customer(String name) {
+          this.name = name;
+      }
+  
+      public void addRental(Rental rental) {
+          rentals.add(rental);
+      }
+  
+      public String getName() {
+          return name;
+      }
+  
+      public String statement() {
+          double totalAmount = 0;
+          int frequentRenterPoints = 0;
+          StringBuilder result = new StringBuilder("Rental Record for " + getName() + "\n");
+  
+          for (Rental rental : rentals) {
+              // Substituímos a chamada para amountFor pelo método getCharge
+              double thisAmount = rental.getCharge();
+  
+              // Adiciona pontos de aluguel frequente
+              frequentRenterPoints++;
+              if (rental.getMovie().getPriceCode() == Movie.NEW_RELEASE && rental.getDaysRented() > 1) {
+                  frequentRenterPoints++;
+              }
+  
+              // Adiciona detalhes do aluguel ao relatório
+              result.append(String.format("\t%s\t%.2f\n", rental.getMovie().getTitle(), thisAmount));
+              totalAmount += thisAmount;
+          }
+  
+          // Adiciona os totais ao final do relatório
+          result.append(String.format("Amount owed is %.2f\n", totalAmount));
+          result.append(String.format("You earned %d frequent renter points", frequentRenterPoints));
+  
+          return result.toString();
+      }
+  
+  
     private double amountFor(Rental aRental) {
       double amount = 0;
   
--- Rental.java
+++ Rental.java
@@ -1,18 +1,42 @@
- public class Rental {
+public class Rental {
+    private Movie movie;
+    private int daysRented;
 
-    private Movie _movie;
-    private int _daysRented;
- 
     public Rental(Movie movie, int daysRented) {
-       _movie = movie;
-       _daysRented = daysRented;
+        this.movie = movie;
+        this.daysRented = daysRented;
     }
- 
+
     public int getDaysRented() {
-       return _daysRented;
+        return daysRented;
     }
- 
+
     public Movie getMovie() {
-       return _movie;
+        return movie;
     }
- } 
+
+    // Novo método getCharge()
+    public double getCharge() {
+        double amount = 0;
+
+        switch (movie.getPriceCode()) {
+            case Movie.REGULAR:
+                amount += 2;
+                if (daysRented > 2) {
+                    amount += (daysRented - 2) * 1.5;
+                }
+                break;
+            case Movie.NEW_RELEASE:
+                amount += daysRented * 3;
+                break;
+            case Movie.CHILDRENS:
+                amount += 1.5;
+                if (daysRented > 3) {
+                    amount += (daysRented - 3) * 1.5;
+                }
+                break;
+        }
+
+        return amount;
+    }
+}
