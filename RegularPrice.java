4ff115a85f793870c1b3a1ef37d7b2be541c6ac2
Arthur Tenorio <arthurtenorio0101@gmail.com>
2024-11-26 15:21:08 -0300

    Commit 13

--- RegularPrice.java
+++ RegularPrice.java
@@ -6,10 +6,9 @@ public class RegularPrice extends Price {
 
     @Override
     public double getCharge(int daysRented) {
-        double amount = 2;
-        if (daysRented > 2) {
-            amount += (daysRented - 2) * 1.5;
-        }
-        return amount;
+        double result = 2;
+        if (daysRented > 2)
+           result += (daysRented - 2) * 1.5;
+        return result;;
     }
 }
