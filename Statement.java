e7020bf73f24163f4f7cfe2afb6919b6af950d0b
Arthur Tenorio <arthurtenorio0101@gmail.com>
2024-11-26 15:28:21 -0300

    Commit 17

--- Statement.java
+++ Statement.java
@@ -1,3 +1,17 @@
 public abstract class Statement {
-    public abstract String value(Customer aCustomer);
+    public String value(Customer aCustomer) {
+        String result = headerString(aCustomer);
+        
+        for (Enumeration rentals = aCustomer.getRentals(); rentals.hasMoreElements();) {
+            Rental each = (Rental) rentals.nextElement();
+            result += rentalString(each);
+        }
+
+        result += footerString(aCustomer);
+        return result;
+    }
+
+    protected abstract String headerString(Customer aCustomer);
+    protected abstract String rentalString(Rental aRental);
+    protected abstract String footerString(Customer aCustomer);
 }
